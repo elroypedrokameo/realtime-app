3c57150edf1f056a86dbaa7be87a940a453ed0ab
Elroy Pedro Kameo <elroypedrokameo10@gmail.com>
2023-08-31 23:11:29 +0700

    feat: authentication with clerk

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 const LayoutPage = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+    <div className="h-full flex items-center justify-center">
       {children}
     </div>
   );
