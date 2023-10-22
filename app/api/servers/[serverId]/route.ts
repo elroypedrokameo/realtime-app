3a23fa1a5c00e14d81dd96ccd67c294c9501f3e2
Elroy Pedro Kameo <elroypedrokameo10@gmail.com>
2023-10-22 21:33:19 +0700

    feat: leave & delete server

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -26,9 +26,34 @@ export async function PATCH(
       },
     });
 
-    return NextResponse.json(server)
+    return NextResponse.json(server);
   } catch (error) {
-    console.log("[ERROR PATCH SERVER]");
+    console.log("[ERROR PATCH SERVER]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[ERROR PATCH DELETE]", error);
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
